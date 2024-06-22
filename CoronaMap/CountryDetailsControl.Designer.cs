bc2f211250506ce246a20e973f28f0cf0ed52f74
jsten <jsten@DESKTOP-HB5B8D2>
2024-06-22 15:15:50 +0200

    Add project files.

---
+++ CoronaMap/CountryDetailsControl.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace CoronaMap
+{
+    partial class CountryDetailsControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelCountryName = new System.Windows.Forms.Label();
+            this.labelCritical = new System.Windows.Forms.Label();
+            this.labelRecovered = new System.Windows.Forms.Label();
+            this.labelDeaths = new System.Windows.Forms.Label();
+            this.labelInfected = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelLongitude = new System.Windows.Forms.Label();
+            this.labelLatitude = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ziek:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Overleden:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hersteld:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Krietiek:";
+            // 
+            // labelCountryName
+            // 
+            this.labelCountryName.AutoSize = true;
+            this.labelCountryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCountryName.Location = new System.Drawing.Point(3, 0);
+            this.labelCountryName.Name = "labelCountryName";
+            this.labelCountryName.Size = new System.Drawing.Size(125, 16);
+            this.labelCountryName.TabIndex = 4;
+            this.labelCountryName.Text = "COUNTRYNAME";
+            // 
+            // labelCritical
+            // 
+            this.labelCritical.AutoSize = true;
+            this.labelCritical.Location = new System.Drawing.Point(80, 45);
+            this.labelCritical.Name = "labelCritical";
+            this.labelCritical.Size = new System.Drawing.Size(15, 13);
+            this.labelCritical.TabIndex = 8;
+            this.labelCritical.Text = " \\";
+            // 
+            // labelRecovered
+            // 
+            this.labelRecovered.AutoSize = true;
+            this.labelRecovered.Location = new System.Drawing.Point(80, 71);
+            this.labelRecovered.Name = "labelRecovered";
+            this.labelRecovered.Size = new System.Drawing.Size(15, 13);
+            this.labelRecovered.TabIndex = 7;
+            this.labelRecovered.Text = " \\";
+            // 
+            // labelDeaths
+            // 
+            this.labelDeaths.AutoSize = true;
+            this.labelDeaths.Location = new System.Drawing.Point(80, 58);
+            this.labelDeaths.Name = "labelDeaths";
+            this.labelDeaths.Size = new System.Drawing.Size(15, 13);
+            this.labelDeaths.TabIndex = 6;
+            this.labelDeaths.Text = " \\";
+            // 
+            // labelInfected
+            // 
+            this.labelInfected.AutoSize = true;
+            this.labelInfected.Location = new System.Drawing.Point(80, 32);
+            this.labelInfected.Name = "labelInfected";
+            this.labelInfected.Size = new System.Drawing.Size(15, 13);
+            this.labelInfected.TabIndex = 5;
+            this.labelInfected.Text = " \\";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Breedtegraad:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 101);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Lengtegraad:";
+            // 
+            // labelLongitude
+            // 
+            this.labelLongitude.AutoSize = true;
+            this.labelLongitude.Location = new System.Drawing.Point(80, 101);
+            this.labelLongitude.Name = "labelLongitude";
+            this.labelLongitude.Size = new System.Drawing.Size(15, 13);
+            this.labelLongitude.TabIndex = 12;
+            this.labelLongitude.Text = " \\";
+            // 
+            // labelLatitude
+            // 
+            this.labelLatitude.AutoSize = true;
+            this.labelLatitude.Location = new System.Drawing.Point(80, 88);
+            this.labelLatitude.Name = "labelLatitude";
+            this.labelLatitude.Size = new System.Drawing.Size(15, 13);
+            this.labelLatitude.TabIndex = 11;
+            this.labelLatitude.Text = " \\";
+            // 
+            // CountryDetailsControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.labelLongitude);
+            this.Controls.Add(this.labelLatitude);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelCritical);
+            this.Controls.Add(this.labelRecovered);
+            this.Controls.Add(this.labelDeaths);
+            this.Controls.Add(this.labelInfected);
+            this.Controls.Add(this.labelCountryName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CountryDetailsControl";
+            this.Size = new System.Drawing.Size(177, 120);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelCountryName;
+        private System.Windows.Forms.Label labelCritical;
+        private System.Windows.Forms.Label labelRecovered;
+        private System.Windows.Forms.Label labelDeaths;
+        private System.Windows.Forms.Label labelInfected;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelLongitude;
+        private System.Windows.Forms.Label labelLatitude;
+    }
+}
---
+++ CoronaMap/CountryDetailsControl.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CoronaMap.Data;
+using Newtonsoft.Json;
+using CoronaMap.Properties;
+using System.Resources;
+
+namespace CoronaMap
+{
+    public partial class CountryDetailsControl : UserControl
+    {
+        //1,2,3
+        public int ControlSize { get; set; }
+
+
+        public CountryDetailsControl()
+        {
+            InitializeComponent();
+            ControlSize = 2;
+        }
+
+        public void LoadDetails(CountryTotal totals)
+        {
+            if (totals.location.Length > 15)
+            {
+                char[] c = totals.location.ToCharArray();
+                int i = totals.location.LastIndexOf(" ");
+                c[i] = '\n';
+
+                labelCountryName.Text = new string(c);
+            }
+            else
+            {
+                labelCountryName.Text = totals.location;
+            }
+
+            //labelCountryName.Text = totals.country;
+            labelCritical.Text = totals.icu_patients.ToString();
+            labelDeaths.Text = totals.total_deaths.ToString();
+            labelInfected.Text = totals.total_cases.ToString();
+            labelRecovered.Text = "todo";
+            labelLatitude.Text = totals.latitude.ToString();
+            labelLongitude.Text = totals.longitude.ToString();
+            //return;
+
+            switch (ControlSize)
+            {
+                case 1:
+                    {
+                        this.Width = 177;
+                        this.Height = 92;
+                        break;
+                    }
+                case 2:
+                    {
+                        this.Width = 177;
+                        this.Height = 120;
+                        break;
+                    }
+                case 3:
+                    {
+                        this.Width = 250;
+                        this.Height = 120;
+                        break;
+                    }
+                default:
+                    break;
+            }
+
+
+        }
+    }
+}
